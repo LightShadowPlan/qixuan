43f0a4c3ce36c5415ab7e1c6c6752520fdc2d03d
LightShadowPlan <2103875834@qq.com>
2018-12-03 09:13:51 +0800

    '重新规划'

--- src/javascripts/controllers/index.js
+++ src/javascripts/controllers/index.js
@@ -11,36 +11,12 @@ const render = () => {
         return 'rgb(' + R + ',' + G + ',' + B
         ')';
     }
-
     setInterval(function () {
         topTitle.style.color = randomColor();
     }, 5000)
-    // 侧边栏定位
-    $('.left').on('click', '.left-list', function () {
-        let index = $(this).index()
-        let scrollY = $('.center .center-list').eq(index).offset().top
-        $('html').animate({scrollTop: scrollY})
-        $(this).addClass('leftSelected').siblings().removeClass('leftSelected')
-    })
-    //页面滚动改变侧边栏状态
-    let scrollArray = []
-    for( let i=1; i<$('.left .left-list').length; i++){
-        scrollArray.push($('.center .center-list').eq(i).offset().top)
-    }
-    let timer = null
-    $(window).on('scroll', throttle(function(){
-        let htmlScroll = $(window).scrollTop() + 130
-        for(let i = 0; i< scrollArray.length; i++){
-            if( htmlScroll < scrollArray[i] ){
-                $('.left .left-list').eq(i).addClass('leftSelected').siblings().removeClass('leftSelected')
-                break;
-            } else if( i === scrollArray.length-1 ){
-                $('.left .left-list').eq(i+1).addClass('leftSelected').siblings().removeClass('leftSelected')
-            }
 
+    //导航栏
 
-        }
-    },100))
     //函数节流
     function throttle(callback,duration,content){
         var lasttime = 0;
