9c4059a5c7e6e7b8d563e70a064fa19c394abf81
LightShadowPlan <2103875834@qq.com>
2018-12-11 15:09:25 +0800

    '重构'

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### 这是一个轻量级的css响应式框架
 - - -
-[查看详细文档](http://likeshadow.gitee.io/qixuan)
+[查看详细文档](https://www.lightshadow.xyz/github/qixuan)
--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,5 +1,12 @@
 
-import render from './controllers/index'
-
-render()
+    //头部渐变title
+    function randomColor() {
+        let R = parseInt(Math.random() * 255);
+        let G = parseInt(Math.random() * 255);
+        let B = parseInt(Math.random() * 255);
+        return 'rgb(' + R + ',' + G + ',' + B +')';
+    }
+    setInterval(function () {
+        topTitle.style.color = randomColor();
+    }, 5000)
 
